9ff95bdc1d44f99ee55af4452a12db99537ee343
loglogc-1257 <fadonougbosebastien7@gmail.com>
2025-04-22 15:47:13 +0100

    Create de.js

---
+++ commands/de.js
@@ -0,0 +1,52 @@
+const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
+const { sendMessage } = require('../handles/sendMessage');
+
+module.exports = {
+  name: 'de',
+  description: "Génère une image avec DALL·E via SDXL",
+  usage: 'de [prompt]',
+  author: 'vex_Kshitiz',
+
+  async execute(senderId, args, pageAccessToken, { message, event, api }) {
+    api.setMessageReaction("🕐", event.messageID, (err) => {}, true);
+    try {
+      const baseUrl = "https://kshitiz-t2i-x6te.onrender.com/sdxl";
+      let prompt = '';
+      const model_id = 33;
+
+      if (args.length > 0) {
+        prompt = args.join(" ").trim();
+      } else {
+        return message.reply("❌ | Please provide a prompt.");
+      }
+
+      const apiResponse = await axios.get(baseUrl, {
+        params: {
+          prompt: prompt,
+          model_id: model_id
+        }
+      });
+
+      if (apiResponse.data.imageUrl) {
+        const imageUrl = apiResponse.data.imageUrl;
+        const imagePath = path.join(__dirname, "cache", `de.png`);
+        const imageResponse = await axios.get(imageUrl, { responseType: "stream" });
+        const imageStream = imageResponse.data.pipe(fs.createWriteStream(imagePath));
+        imageStream.on("finish", () => {
+          const stream = fs.createReadStream(imagePath);
+          message.reply({
+            body: "",
+            attachment: stream
+          });
+        });
+      } else {
+        throw new Error("Image URL not found in response");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      message.reply("❌ | An error occurred. Please try again later.");
+    }
+  }
+};
